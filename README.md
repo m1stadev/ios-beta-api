a97950d318c9d40b48b2a5bab5cbc3df6001aabf
m1stadev <37033149+m1stadev@users.noreply.github.com>
2021-07-01 22:36:09 -0500

    Add script

---
+++ README.md
@@ -0,0 +1,12 @@
+# Beta Firmware API
+This is a python script that scrapes information on beta iOS firmwares from [The iPhone Wiki](https://www.theiphonewiki.com/), fetches the signing status of the firmwares, then writes the data out as JSON, in a format similar to [IPSW.me](https://ipswdownloads.docs.apiary.io/)'s API. 
+
+## Requirements
+- A computer running macOS or Linux
+- An Internet connection
+- [tsschecker](https://github.com/1Conan/tsschecker)
+- Pip dependencies:
+    - `pip3 install -r requirements.txt`
+
+## Support
+For support, open an [issue](https://github.com/m1stadev/beta-firmware-API/issues/new), or join my [Discord server](https://m1sta.xyz/discord).
---
+++ wiki-scraper.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python3
+
+from concurrent.futures import ThreadPoolExecutor
+from mwclient import Site
+import json
+import os
+import plistlib
+import re
+import remotezip
+import shutil
+import subprocess
+import tempfile
+import time
+
+
+class BetaScraper(object):
+    def __init__(self, site):
+        self.site = site
+        self.api = dict()
+
+    def grab_pages(self, filters):
+        pages = list()
+        for result in self.site.search('Beta Firmware/'):
+            if any(x in result['title'] for x in filters) and '.x' in result['title']:
+                pages.append(result['title'])
+
+        return pages
+
+    def grab_urls(self, pages):
+        urls = list()
+        for page in pages:
+            page_regex = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', self.site.pages[page].text())
+            [urls.append(url) for url in page_regex if url.endswith('.ipsw') and url not in urls]
+
+        return urls
+
+    def scrape_firm(self, url):
+        if any(domain in url for domain in ('developer.apple.com', 'adcdownload.apple.com')): # Inaccessible domains
+            return
+
+        with tempfile.TemporaryDirectory() as tmpdir:
+            try:
+                with remotezip.RemoteZip(url) as ipsw:
+                    manifest = next(f for f in ipsw.namelist() if 'Manifest' in f)
+                    ipsw.extract(manifest, tmpdir)
+            except:
+                return
+
+            with open('/'.join((tmpdir, manifest)), 'rb') as f:
+                bm = plistlib.load(f)
+
+            args = (
+                'tsschecker',
+                '-B',
+                bm['BuildIdentities'][0]['Info']['DeviceClass'],
+                '-m',
+                f'{tmpdir}/{manifest}'
+            )
+
+            tsschecker = subprocess.run(args, stdout=subprocess.PIPE, universal_newlines=True)
+
+        for device in bm['SupportedProductTypes']:
+            if device not in self.api.keys():
+                self.api[device] = list()
+
+            self.api[device].append({
+                'version': bm['ProductVersion'],
+                'buildid': bm['ProductBuildVersion'],
+                'url': url,
+                'signed': True if 'IS being signed!' in tsschecker.stdout else False
+            })
+
+    def write_api(self, path):
+        if os.path.exists(path):
+            shutil.rmtree(path)
+
+        os.mkdir(path)
+        for device in self.api.keys():
+            with open(f'{path}/{device}', 'w') as f:
+                json.dump(sorted(self.api[device], key=lambda firm: firm['version'], reverse=True), f)
+
+def main():
+    start_time = time.time()
+    device_types = ('Apple TV', 'iPad', 'iPad Air', 'iPad Pro', 'iPad Mini', 'iPhone', 'iPod touch')
+    scraper = BetaScraper(Site('www.theiphonewiki.com'))
+
+    print('[1] Scraping The iPhone Wiki...')
+    pages = scraper.grab_pages(device_types)
+
+    print('[2] Scraping beta IPSW URLs...')
+    urls = scraper.grab_urls(pages)
+
+    print('[3] Grabbing rest of info from IPSWs (this will take a while, please wait)...')
+    with ThreadPoolExecutor() as executor:
+        for url in urls:
+            executor.submit(scraper.scrape_firm, url)
+
+    print('[4] Writing out API...')
+    scraper.write_api('betas')
+
+    print(f'Done! Took {round(time.time() - start_time)}s.')
+
+if __name__ == "__main__":
+    main()
